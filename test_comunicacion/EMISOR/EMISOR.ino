6f153eba4db7d6b6ac8ea5b3a573c7e8a49c2385
Bojtronic <bojtronic@gmail.comc>
2026-02-26 22:10:51 -0600

    Prueba de comunicación con ESP-NOW

--- test_comunicacion/EMISOR/EMISOR.ino
+++ test_comunicacion/EMISOR/EMISOR.ino
@@ -1,7 +1,7 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-uint8_t direccionReceptor[] = {0xXX,0xXX,0xXX,0xXX,0xXX,0xXX}; // PONER MAC AQUÍ
+uint8_t direccionReceptor[] = {0x20, 0xE7, 0xC8, 0xAC, 0x05, 0x70};
 
 const int boton = 25;
 
@@ -11,8 +11,15 @@ typedef struct struct_message {
 
 struct_message mensaje;
 
+// firma para core 3.x
+void OnDataSent(const wifi_tx_info_t *info, esp_now_send_status_t status) {
+  Serial.print("Estado de envio: ");
+  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "OK" : "FALLO");
+}
+
 void setup() {
   Serial.begin(115200);
+  delay(1000);
 
   pinMode(boton, INPUT_PULLUP);
 
@@ -23,6 +30,8 @@ void setup() {
     return;
   }
 
+  esp_now_register_send_cb(OnDataSent);
+
   esp_now_peer_info_t peerInfo = {};
   memcpy(peerInfo.peer_addr, direccionReceptor, 6);
   peerInfo.channel = 0;
@@ -32,12 +41,15 @@ void setup() {
     Serial.println("Error agregando peer");
     return;
   }
+
+  Serial.println("Emisor listo");
 }
 
 void loop() {
   bool estadoBoton = digitalRead(boton) == LOW;
 
   mensaje.estado = estadoBoton;
+
   esp_now_send(direccionReceptor, (uint8_t *) &mensaje, sizeof(mensaje));
 
   delay(100);
--- test_comunicacion/GET_MAC/GET_MAC.ino
+++ test_comunicacion/GET_MAC/GET_MAC.ino
@@ -3,7 +3,11 @@
 void setup(){
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
-  Serial.println(WiFi.macAddress());
 }
 
-void loop(){}
+void loop(){
+  Serial.println("MAC Address:");
+  Serial.println(WiFi.macAddress());
+  Serial.println("");
+  delay(10000);
+}
